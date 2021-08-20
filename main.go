a0b3f129642be7b140696749db91dca2100be40b
Lucas Machado <lucasml.bh@gmail.com>
2021-08-20 17:44:48 -0300

    Correção nos arquivos de cloud

--- main.go
+++ main.go
@@ -9,5 +9,4 @@ func main() {
 	loc, _ := time.LoadLocation("America/Sao_Paulo")
 	fmt.Println("Hora UTC:", time.Now())
 	fmt.Println("Hora UTC-3:", time.Now().In(loc))
-	time.Sleep(time.Minute * 10)
 }
